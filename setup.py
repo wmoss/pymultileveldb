a6d7d23dfe618701b5c737807228eba595b12122
Will Moss <wmoss@bu.mp>
2013-08-16 21:34:06 -0700

    A simple Python client for MultilevelDB

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+VERSION = "0.1"
+
+setup(name="pymultileveldb",
+      version=VERSION,
+      author="Will Moss",
+      author_email="wbmoss@gmail.com",
+      packages=["pymultileveldb"],
+      install_requires=[],
+)
